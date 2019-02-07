73f34c72474009ad7219a87d84e9d23d6d735762
Charles Ferreira <charlesdccti@gmail.com>
2019-02-07 01:57:03 -0300

    Adicionando método finAll() com parginacao

--- src/main/java/br/com/produto/controllers/ProdutoResource.java
+++ src/main/java/br/com/produto/controllers/ProdutoResource.java
@@ -9,6 +9,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,7 +31,13 @@ import br.com.produto.repositories.ProdutoRepository;
 public class ProdutoResource {
 
 	@Autowired
-	private ProdutoRepository repo;
+	private ProdutoRepository repo; 	//crud simples, praticamente sem regra de negócio alguma.
+	
+	@GetMapping
+	public ResponseEntity<?> findAll(@RequestParam("page") int page,  @RequestParam("size") int size) {
+		Page<Produto> produtos = repo.findAll(PageRequest.of(page, size));
+		return ResponseEntity.ok(produtos);
+	}
 	
 	@GetMapping(value="{id}")
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
